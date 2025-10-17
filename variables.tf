944eacb748ab0a3a0b0b815ba658460dad8eea9d
Javier Soriano <114910690+sorianfr@users.noreply.github.com>
2025-10-17 15:35:57 +0200

    Define VPC and public subnet CIDR block variables
    Added variables for VPC and public subnet CIDR blocks.

--- variables.tf
+++ variables.tf
@@ -10,6 +10,16 @@ variable "availability_zone" {
   description = "Availability zone for the public subnet"
 }
 
+variable "vpc_cidr_block" {
+  description = "CIDR block for the shared VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR block for the shared Public Subnet"
+  default     = "10.0.1.0/24"
+}
+
 variable "ami_id" {
   description = "AMI ID for the EC2 instances"
   type        = string
